f5131e7d6d398c5bc279e7aaaaff7158eed8013e
roufhasane <roufhasan5@gmail.comm>
2023-06-13 18:32:27 +0600

    users role update method applied

--- index.js
+++ index.js
@@ -49,6 +49,34 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // All Classes
     app.get("/classes", async (req, res) => {
       const result = await classesCollection.find().toArray();
